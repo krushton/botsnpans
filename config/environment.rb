5759da2ab8a23fd5adf6a8e632da2ac00b508879
krushton <kjrushton@gmail.com>
2012-11-05 18:32:50 -0800

    devise is now working with email from sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Botsnpans::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => Botsnpans::Application.config.sendgrid_user,
+  :password => Botsnpans::Application.config.sendgrid_pass,
+  :domain => "botsandpans.com",
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
