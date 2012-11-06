e425fd526401dd4d3ebe30ae3cb4ead66bad3392
krushton <kjrushton@gmail.com>
2012-11-05 17:48:55 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Botsnpans::Application.config.secret_token = 'f802539d835dcd85850fd323c0c2b90f7888f08e9504182d04b930285d76b12edacf9e81d7bf4a698c6807caac2767810905c2b1b372498621b81b6e8fdda3ae'
