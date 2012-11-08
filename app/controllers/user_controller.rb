95389f3896912dc42f199c9f3a32f3908c452598
krushton <kjrushton@gmail.com>
2012-11-08 11:36:09 -0800

    add user controller

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,7 @@
+class UserController < ApplicationController
+  def all
+  end
+
+  def show
+  end
+end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,4 @@
 class Game < ActiveRecord::Base
 	has_many :level_data_items
+	belongs_to :user
 end
