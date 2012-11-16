bece46588d89aeac6cf0dc5b7badc56887420e7e
krushton <kjrushton@gmail.com>
2012-11-16 00:53:19 -0800

    registration controller issues

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,20 +5,9 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def create
-    @user = User.new(params[:user])
-    @user.robot = Robot.new
-
-    respond_to do |format|
-      if @user.save
-        flash[:notice] = "New user added"
-        format.html { redirect_to root_url }
-        format.json { render json: @user, status: :created, location: @user }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+  	super
   end
-
+  
   def update
     super
   end
