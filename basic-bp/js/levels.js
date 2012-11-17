d50f81510eb8cb5c622937ddbfa1b3fadb4258ee
Sandra Helsley <syh@ischool.berkeley.edu>
2012-11-17 15:39:02 -0800

    More CSS fixes, added buttons

--- basic-bp/js/levels.js
+++ basic-bp/js/levels.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function() {
-    $( ".accordion" ).accordion({
+$( ".accordion" ).accordion({
     	collapsible: true,
-    	heightStyle: "fill"
+    	heightStyle: "fill",
     });
 });
