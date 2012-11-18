2d90ce360e82ef2e7d3add10dc3be23070024caf
Morgan <Morgan@Morgans-MacBook-Pro.local>
2012-11-18 14:23:12 -0800

    Modal welcome screen updated and styled

--- basic-bp/js/modal.js
+++ basic-bp/js/modal.js
@@ -2,10 +2,22 @@ $(function() {
         $( "#modalIntro" ).dialog({
             modal: true,
             width: '400px',
+            open : function(event, ui) {$(".ui-widget-overlay").bind('click', function(){$("#modalIntro").dialog('close'); });},
             buttons: {
-                Ok: function() {
+                "Let's Get Cooking": function() {
                     $( this ).dialog( "close" );
+                    $(this).addClass("greenButton")
                 }
-            }
+            },
+            // create:function () {
+            //     $(this).closest("#modalIntro")
+            //     .find(".ui-button:first") // the first button
+            //     ;
+            // }
         });
     });
+$(document).ready(function(){
+    $('.ui-icon').hide();
+    $(this).find('.ui-button').addClass("greenButton")
+    }
+)
