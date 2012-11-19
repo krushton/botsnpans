c23dcff033e09a8cdc9ff8070b6be96bd14596a9
Morgan <Morgan@Morgans-MacBook-Pro.local>
2012-11-18 17:47:57 -0800

    'edit-robot' updated with bootstrap

--- basic-bp/js/edit-robot.js
+++ basic-bp/js/edit-robot.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
 	$('#editName').hide();
+	$('#editNameBtn').on('click',function(){
+		$('#editNameBtn').hide(200);
+		$('#editName').show();
+	});
+	$('#nameUpdated').on('click',function(){
+		var newUsername = $("#newName").val()
+		$('#userName').html(newUsername);
+		$('#editName').hide();
+	});
 })
