fc25b3aeff3cfa2a751ac968f4a6e2c539045129
krushton <kjrushton@gmail.com>
2012-11-18 18:14:01 -0800

    added tutorial level

--- basic-bp/js/game.js
+++ basic-bp/js/game.js
@@ -1,21 +1,35 @@
 $(document).ready(function() {
-	
+      	
+      levels = {
+      "Tutorial" : [ 
+           { url : 'water.jpg', id : 1, type : 'ingredient', parents: [0,0]},
+           { url : 'pot.jpg', id : 2, type : 'tool',  parents: [0,0]},
+           { url : 'cake.jpg', id : 3, type : 'ingredient',  parents: [0,0], last: true},
+           { url: 'bowl.jpg', id : 4, type: 'tool', parents: [0,0]},
+           { url : 'potwithwater.jpg', id : 5, type : 'transition',  parents: [1,2]}
+      ],
+      "Ramen" : [ 
+           { url : 'water.jpg', id : 1, type : 'ingredient', parents: [0,0]},
+           { url : 'ramen.jpg', id : 2, type : 'ingredient', parents: [0,0]},
+           { url : 'pot.jpg', id : 3, type : 'tool',  parents: [0,0]},
+           { url : 'heat.jpg', id : 4, type : 'transition',  parents: [0,0]},
+           { url : 'potwithwater.jpg', id: 5, type : 'transition', parents: [1, 3] },
+           { url : 'boilingwater.jpg', id: 6, type : 'transition', parents: [4, 5] },
+           { url : 'rameninpot.jpg', id: 7, type : 'transition', parents: [6, 2] },
+           { url : 'rameninbowl.jpg', id: 8, type: 'transition', parents: [7, 9], last: true},
+           { url: 'bowl.jpg', id : 9, type: 'tool', parents: [0,0]}
+      ]
+      };
 
-states = [ 
-     { url : 'water.jpg', id : 1, type : 'ingredient', parents: [0,0]},
-     { url : 'ramen.jpg', id : 2, type : 'ingredient', parents: [0,0]},
-     { url : 'pot.jpg', id : 3, type : 'tool',  parents: [0,0]},
-     { url : 'heat.jpg', id : 4, type : 'transition',  parents: [0,0]},
-     { url : 'potwithwater.jpg', id: 5, type : 'transition', parents: [1, 3] },
-     { url : 'boilingwater.jpg', id: 6, type : 'transition', parents: [4, 5] },
-     { url : 'rameninpot.jpg', id: 7, type : 'transition', parents: [6, 2] },
-     { url : 'rameninbowl.jpg', id: 8, type: 'transition', parents: [7, 9], last: true},
-     { url: 'bowl.jpg', id : 9, type: 'tool', parents: [0,0]}
-];
-
-
-      loadStates();
-
+      tutorialMessages = [{title: "Stove", message: "Put the pot on the stove.", source: '2', location: 'stoveTop'},
+      {title:"Combine", message: "Put the water in the pot.", source: '1', location: '2'},
+      {title: "Workspace", message: "Put the cake in the workspace.", source: '3', location: "workSpace"},
+      {title: "Serve", message: "Serve the cake!", source: '3', location: "botSpace"},
+      {title: "Tutorial Complete", message: "Great Job!" }]
+      states = [];
+      level = '';
+      tutorialStep = 0;
+      init();
 
      $('.item').css('z-index', 100);
 
@@ -27,6 +41,7 @@ states = [
         accepts : '.item',
         greedy : true,
         drop : function(event, ui) {
+          console.log(level);
           handleDrop(event,ui);
         }         
      });
@@ -44,22 +59,34 @@ states = [
                  .droppable({accepts: '.item', greedy: true, drop: handleDrop});
                  $(this).append(elem);
             }
+            if (level == "Tutorial") {
+                handleTutorial(id, $(this).attr('id'));
+          }
+          
         }
      });
 
      $('#serve').click(function() {
 
-        for (var i = 0; i < states.length; i++ ){
-          if (states[i].last) 
-          {
-            var finalId = states[i].id;
-          }
-        }
-            $('.item').each(function() {
-            if (parseInt(stripClone($(this).attr('id'))) == finalId) {
-              $('#botSpace').append($(this));
+          var finalId = 0;
+
+          for (var i = 0; i < states.length; i++ ){
+            if (states[i].last) 
+            {
+              finalId = states[i].id;
             }
-        });
+          }
+
+          $('.item').each(function() {
+              if (parseInt(stripClone($(this).attr('id'))) == finalId) {
+                if (level == "Tutorial") {
+                    handleTutorial($(this).attr('id'), "botSpace");
+                  }
+                 $('#botSpace').find('.item').remove();
+                 $('#botSpace').append($(this).css('position', 'static'));
+
+              }
+          });
      });
 
 
@@ -88,22 +115,38 @@ states = [
 
 
 function handleDrop(event, ui) {
-            var first = $(ui.draggable).attr('id');
-            var second = $(this).attr('id');
-            console.log(first + " " + second);
 
-            for (var i=0; i < states.length; i++) {
-                var intone = parseInt(stripClone(first));
-                var inttwo = parseInt(stripClone(second));
+     var first = $(ui.draggable).attr('id');
+     var second = $(this).attr('id');
 
-              if (states[i].parents.indexOf(intone) >= 0 && states[i].parents.indexOf(inttwo) >= 0) {
-                $('#' + first).remove();
-                $('#' + second).attr({'src': 'images/items/' + states[i].url, 'id' : states[i].id});
-                break;
-              }
-            
-            }
+
+   for (var i=0; i < states.length; i++) {
+      var intone = parseInt(stripClone(first));
+      var inttwo = parseInt(stripClone(second));
+
+    if (states[i].parents.indexOf(intone) >= 0 && states[i].parents.indexOf(inttwo) >= 0) {
+        $('#' + first).remove();
+        $('#' + second).attr({'src': 'images/items/' + states[i].url, 'id' : states[i].id});
+        break;
       }
+   }
+     if (level == "Tutorial") {
+            handleTutorial(stripClone(first), stripClone(second));
+     }
+          
+ }
+
+
+function handleTutorial(source, target) {
+  
+          if ( tutorialMessages[tutorialStep].source == source && tutorialMessages[tutorialStep].location == target  ) {
+                 updateDialog( tutorialMessages[tutorialStep+1].title, tutorialMessages[tutorialStep+1].message);
+                if (tutorialStep != tutorialMessages.length -1) {
+                 tutorialStep++;
+             }
+          }
+}
+
 
 function stripClone(name) {
   var index = name.indexOf('-');
@@ -117,7 +160,13 @@ function stripClone(name) {
 
 
 
-function loadStates() {
+function init() {
+        hash = window.location.hash;
+        level = hash.substring(1,hash.length);
+        console.log(level);
+        $('#recipeName').text(level);
+        states = levels[level];
+
 	      for (var i = 0; i < states.length; i++ ){
       	if (states[i].type == 'ingredient') {
       		$('#ingredients').append('<img class="item" src="' + 'images/items/' + states[i].url + '" id="' + states[i].id + '">');
@@ -126,4 +175,28 @@ function loadStates() {
       		$('#tools').append('<img class="item" src="'  + 'images/items/' + states[i].url + '" id="' + states[i].id + '">');
       	}
       }
+
+      if (level == 'Tutorial') {
+        $('#dialog').dialog({
+          autoOpen: true,
+          hide: 'fold',
+          title: tutorialMessages[0].title,
+          width: 200,
+          height:120,
+          resizable: false
+        });
+
+        $('#dialog').html('<p id="message">' + tutorialMessages[0].message + '</p>');
+        var closeButton = $('<span class="greenButton" style="width:40px;">OK</span>').bind("click", function() {
+            $('#dialog').dialog('close');
+        });
+        $('#dialog').append(closeButton);
+
+      }
+}
+
+function updateDialog(title, text) {
+ $('#dialog').dialog('option','title', title)
+  .find('#message').text(text);
+  $('#dialog').dialog('open');
 }
