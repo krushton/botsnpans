bc485759b86b0e03731010abd7ca7f159ced6e39
Morgan <Morgan@Morgans-MacBook-Pro.local>
2012-11-19 17:03:16 -0800

    Links between pages work. Modal works on Recipebook.html

--- basic-bp/js/modal.js
+++ basic-bp/js/modal.js
@@ -1,27 +1,6 @@
-$(function() {
-        $( "#modalIntro" ).dialog({
-            modal: true,
-            width: '500px',
-            open : function(event, ui) {$(".ui-widget-overlay").bind('click', function(){$("#modalIntro").dialog('close'); });},
-            buttons: {
-                "Let's Get Cooking": function() {
-                    $( this ).dialog( "close" );
-                    $(this).addClass("greenButton")
-                }
-            },
-            // create:function () {
-            //     $(this).closest("#modalIntro")
-            //     .find(".ui-button:first") // the first button
-            //     ;
-            // }
-        });
-    });
-$(document).ready(function(){
-    $('.ui-icon').hide();
-    $(this).find('.ui-button').addClass("greenButton");
-    $('.ui-button').on('click',function(){
-        alert('test');
-    }) 
-    }
 
-)
+$(document).ready(function(){
+    $('#modalIntro').modal('show')
+    
+    })
+    
