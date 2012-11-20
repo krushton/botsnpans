35f484d93e319aff3c2bc3a61c146ee7fb721c35
krushton <kjrushton@gmail.com>
2012-11-19 22:32:23 -0800

    added pancakes level

--- basic-bp/js/game.js
+++ basic-bp/js/game.js
@@ -18,6 +18,27 @@ $(document).ready(function() {
            { url : 'rameninpot.jpg', id: 7, type : 'transition', parents: [6, 2] },
            { url : 'rameninbowl.jpg', id: 8, type: 'transition', parents: [7, 9], last: true},
            { url: 'bowl.jpg', id : 9, type: 'tool', parents: [0,0]}
+      ],
+      "Pancakes" : [ 
+           { url : 'pancakemix.jpg', id: 1, type: 'ingredient', parents: [0,0]},
+           { url : 'bowl.jpg', id: 2, type: 'tool', parents: [0,0]},
+           { url: 'flourinbowl.jpg', id: 3, type:'transition', parents: [1,2]},
+           { url: 'water.jpg', id: 4, type: 'ingredient', parents: [0,0]},
+           { url: 'wetmix.jpg', id: 5, type:'transition', parents: [3,4]},
+           { url: 'whisk.jpg', id: 14, type: 'tool', parents: [0,0]},
+           { url: 'batter.jpg', id: 15, type: 'transition', parents: [5, 14]},
+           { url: 'fryingpan.jpg', id: 6, type:'tool', parents: [0,0]},
+           { url: 'butter.jpg', id: 7, type:'ingredient', parents: [0,0]},
+           { url: 'fryingpanwithbutter.jpg', id: 8, type:'transition', parents: [6,7]},
+           { url: 'heat.jpg', id: 9, type:'transition', parents: [0,0]},
+           { url: 'hotskillet.jpg', id: 10, type:'transition', parents: [8,9]},
+           { url: 'pancakeinpan.jpg', id: 11, type:'transition', parents: [10,5]},
+           { url: 'spatula.jpg', id: 16, type:'tool', parents: [0,0]},
+           { url: 'flippedpancake.jpg', id: 17, type:'transition', parents: [11,16]},
+           { url: 'plate.jpg', id: 12, type:'tool', parents: [0,0]},
+           { url: 'pancakeonplate.jpg', id: 13, type:'transition', parents: [17,12]},
+           { url: 'syrup.jpg', id: 18, type:'ingredient', parents: [0,0]},
+           { url: 'pancakestack.jpg', id: 19, type:'transition', parents: [13,18], last:true},
       ]
       };
 
@@ -43,7 +64,6 @@ $(document).ready(function() {
         accepts : '.item',
         greedy : true,
         drop : function(event, ui) {
-          console.log(level);
           handleDrop(event,ui);
         }         
      });
@@ -56,7 +76,7 @@ $(document).ready(function() {
                  var elem = ui.helper.clone()
                  .attr('id', id + "-clone")
                  .removeClass('ui-draggable-dragging')
-                 .css({'left': 0, "z-index": 99})
+                 .css({'left': 0, 'z-index': 99, 'top':20})
                  .draggable()
                  .droppable({accepts: '.item', greedy: true, drop: handleDrop});
                  $(this).append(elem);
@@ -101,13 +121,18 @@ $(document).ready(function() {
                 var item = $(this).parent().find('.item');
 
                 if (item != "undefined" && item.length == 1) {
-                  var id = parseInt(item.attr('id'));
-                   for (var i =0; i < states.length; i++) {
-                    if (states[i].parents.indexOf(id) >= 0 && states[i].parents.indexOf(4) >= 0) {
-                           $('#' + id).attr({'src': 'images/items/' + states[i].url, 'id' : states[i].id});
-                           break;
-                    }
+                  var origId = item.attr('id');
+                  var id = parseInt(stripClone(item.attr('id')));
 
+                  var heatId = parseInt(lookupHeat());
+
+                   for (var i =0; i < states.length; i++) {
+
+                       if (states[i].parents.indexOf(id) >= 0 && states[i].parents.indexOf(heatId) >= 0) {
+
+                           $('#' + origId).attr({'src': 'images/items/' + states[i].url, 'id' : states[i].id});
+                           break;
+                        }
                    }
                 }
 
@@ -115,23 +140,53 @@ $(document).ready(function() {
     });
 });
 
+function lookupHeat() {
+
+  for (var i = 0; i < states.length; i++) {
+
+    if (states[i].url.indexOf('heat') >= 0) {
+      return states[i].id;
+    }
+  }
+}
 
 function handleDrop(event, ui) {
 
-     var first = $(ui.draggable).attr('id');
-     var second = $(this).attr('id');
+      var first = $(ui.draggable).attr('id');
+      var second = $(this).attr('id');
+      var match = false;
+      var position = $(this).position();
 
-
-   for (var i=0; i < states.length; i++) {
       var intone = parseInt(stripClone(first));
       var inttwo = parseInt(stripClone(second));
+      console.log(position);
 
-    if (states[i].parents.indexOf(intone) >= 0 && states[i].parents.indexOf(inttwo) >= 0) {
-        $('#' + first).remove();
-        $('#' + second).attr({'src': 'images/items/' + states[i].url, 'id' : states[i].id});
-        break;
-      }
-   }
+       for (var i=0; i < states.length; i++) {
+    
+         if (states[i].parents.indexOf(intone) >= 0 && states[i].parents.indexOf(inttwo) >= 0) {
+            if (first.indexOf('clone') >= 0) {
+              $('#' + first).hide();
+            }
+            $('#' + second).attr({'src': 'images/items/' + states[i].url, 'id' : states[i].id + '-clone'});
+            match = true;
+            break;
+          }
+        }
+
+        var id = $(ui.draggable).attr('id');
+
+        if (!match && id.indexOf('clone') < 0 && $(this).find('#' + id).length == 0) {
+
+            var elem = ui.helper.clone()
+             .attr('id', id + "-clone")
+             .removeClass('ui-draggable-dragging')
+             .css({'left': 0, 'z-index': 99, 'top': 20})
+             .draggable()
+             .droppable({accepts: '.item', greedy: true, drop: handleDrop});
+
+              $(this).after(elem);
+        }
+
      if (level == "Tutorial") {
             handleTutorial(stripClone(first), stripClone(second));
      }
@@ -152,10 +207,9 @@ function handleTutorial(source, target) {
 
 
 function stripClone(name) {
-    var index = name.indexOf('-');
-   if ( index >= 0 ) {
-       return name[0,index-1];
-     }
+    if (name.indexOf('-') >= 0) {
+        return name.split('-')[0];
+    }
     return name;
 }
 
@@ -164,7 +218,7 @@ function stripClone(name) {
 function init() {
         hash = window.location.hash;
         level = hash.substring(1,hash.length);
-        console.log(level);
+
         $('#recipeName').text(level);
         states = levels[level];
 
