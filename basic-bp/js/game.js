8d3df12f78e6481733f9f91d6bec3c5cd433e360
krushton <kjrushton@gmail.com>
2012-11-20 09:06:36 -0800

    make recipe card match level

--- basic-bp/js/game.js
+++ basic-bp/js/game.js
@@ -224,6 +224,7 @@ function init() {
         console.log(level);
         $('#recipeName').text(level);
         states = levels[level];
+        $('#levelCard').src = 'images/levelcards/' + level + ".png";
 
         for (var i = 0; i < states.length; i++ ){
         if (states[i].type == 'ingredient') {
